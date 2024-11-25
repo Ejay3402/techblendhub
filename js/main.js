5404bbe55b25ec2b1d550875b73db6a390b44182
Elijah Jacob <elijahjacob3402@gmail.com>
2024-11-25 13:20:09 +0100

    done

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//payment method
+const enrollBtn = document.getElementById(`enroll-btn`);
+const payment = document.getElementById(`payment`);
+const back = document.getElementById(`back`);
+
+enrollBtn.addEventListener("click", () => payment.style.top = '0');
+
+back.addEventListener("click", () => payment.style.top = '-1000px');
+
+//meun bars open and close 
+
+const meunClose = document.getElementById(`meun-close`);
+const meunOpen = document.getElementById(`meun-open`);
+const meunList = document.getElementById(`meun-list`);
+
+meunOpen.addEventListener("click", () => meunList.style.right = '0');
+meunClose.addEventListener("click", () => meunList.style.right = '-360px');
